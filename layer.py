aaf73070895798284eaf69138f9c8881c6c25713
jyhuang2005 <jyhuang2005@gmail.com>
2022-10-24 12:11:16 -0400

    test

--- layer.py
+++ layer.py
@@ -3,3 +3,6 @@
 class Layer:
     def __init__(self, num_neurons):
         self.num_neurons = num_neurons
+
+    def get_num_neurons(self):
+        return self.num_neurons
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import layer
+
 print('Hello World')
 print('test')
 
@@ -17,20 +19,10 @@ class Neuron:
         return self.value
 
 
-class Layer:
-    def __init__(self, num_neurons):
-        self.num_neurons = num_neurons
-
-
-class Brain:
-    def __init__(self, num_layers, num_neurons):
-        self.num_layers = num_layers
-        self.num_neurons = num_neurons
-
-
-test = Brain(4, [784, 20, 20, 10])
-
 n1 = Neuron(value=5)
 n2 = Neuron(value=6)
 n3 = Neuron(connects=[n1, n2])
 print(n1.get_value(), n2.get_value(), n3.get_value())
+
+l1 = layer.Layer(3)
+print(l1.get_num_neurons())
