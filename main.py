68256ad3a955499d079400e3558af8434c9d8908
jyhuang2005 <jyhuang2005@gmail.com>
2022-10-24 11:15:19 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,32 @@
+print('Hello World')
+print('test')
+
+# added comment
+
+class Neuron:
+    def __init__(self, value):
+        self.value = value
+
+    def __init__(self, connects):
+        self.value = 0
+        for other_neuron in connects:
+            self.value += other_neuron.get_value()
+
+    def get_value(self):
+        return self.get_value()
+
+
+class Layer:
+    def __init__(self, num_neurons):
+        self.num_neurons = num_neurons
+
+
+class Brain:
+    def __init__(self, num_layers, num_neurons):
+        self.num_layers = num_layers
+        self.num_neurons = num_neurons
+
+
+test = Brain(4, [784, 20, 20, 10])
+
+
