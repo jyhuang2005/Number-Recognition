1bc03b693f7841e0c4619711da3405947c298e82
jyhuang2005 <jyhuang2005@gmail.com>
2022-10-24 12:47:11 -0400

    weights

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ l2 = la.Layer(3, l1)
 l3 = la.Layer(5, l2)
 
 for i in l1.get_neuron_array():
-    print(i.get_value())
+    print(i.get_value(), i.get_weights())
 print(l1.get_num_neurons())
 
 for i in l2.get_neuron_array():
-    print(i.get_value())
+    print(i.get_value(), i.get_weights())
 print(l2.get_num_neurons())
 
 for i in l3.get_neuron_array():
-    print(i.get_value())
+    print(i.get_value(), i.get_weights())
 print(l3.get_num_neurons())
+
--- neuron.py
+++ neuron.py
@@ -1,10 +1,19 @@
+import random as r
+
+
 class Neuron:
     def __init__(self, value=0, connects=None):
         if connects is None:
             connects = []
         self.value = value
+        self.weights = []
+        for i in range(0, len(connects)):
+            self.weights.append(r.random())
         for other_neuron in connects:
             self.value += other_neuron.get_value()
 
     def get_value(self):
         return self.value
+
+    def get_weights(self):
+        return self.weights
