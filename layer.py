3b389b3283bde07fb5249a661bfa115a6b38ab07
jyhuang2005 <jyhuang2005@gmail.com>
2022-10-24 12:39:37 -0400

    Layer

--- layer.py
+++ layer.py
@@ -1,6 +1,19 @@
+import neuron as n
+
+
 class Layer:
-    def __init__(self, num_neurons):
+    def __init__(self, num_neurons, prev_layer=None, val_array=None):
         self.num_neurons = num_neurons
+        self.neuron_array = []
+        if prev_layer is None:
+            for i in val_array:
+                self.neuron_array.append(n.Neuron(value=i))
+        if val_array is None:
+            for i in range(0, num_neurons):
+                self.neuron_array.append(n.Neuron(connects=prev_layer.get_neuron_array()))
 
     def get_num_neurons(self):
         return self.num_neurons
+
+    def get_neuron_array(self):
+        return self.neuron_array
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import layer as la
 import neuron as n
 
-print('Hello World')
-print('test')
-
-# added comment
-# justin's contribution!
-
 n1 = n.Neuron(value=5)
 n2 = n.Neuron(value=6)
 n3 = n.Neuron(connects=[n1, n2])
 print(n1.get_value(), n2.get_value(), n3.get_value())
 
-l1 = la.Layer(3)
+l1 = la.Layer(5, val_array=[1, 2, 3, 4, 5])
+l2 = la.Layer(3, l1)
+l3 = la.Layer(5, l2)
+
+for i in l1.get_neuron_array():
+    print(i.get_value())
 print(l1.get_num_neurons())
+
+for i in l2.get_neuron_array():
+    print(i.get_value())
+print(l2.get_num_neurons())
+
+for i in l3.get_neuron_array():
+    print(i.get_value())
+print(l3.get_num_neurons())
