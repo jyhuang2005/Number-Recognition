9c0a22c98f8847b0e05ea601a04817dfb8ede2ee
jyhuang2005 <jyhuang2005@gmail.com>
2022-10-24 13:10:49 -0400

    Added weights

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
 import layer as la
 import neuron as n
 
-n1 = n.Neuron(value=5)
-n2 = n.Neuron(value=6)
-n3 = n.Neuron(connects=[n1, n2])
-print(n1.get_value(), n2.get_value(), n3.get_value())
 
-l1 = la.Layer(5, val_array=[1, 2, 3, 4, 5])
+l1 = la.Layer(5, val_array=[0, 0.2, 0.5])
 l2 = la.Layer(3, l1)
 l3 = la.Layer(5, l2)
 
 for i in l1.get_neuron_array():
-    print(i.get_value(), i.get_weights())
+    print(i.get_value())
 print(l1.get_num_neurons())
 
 for i in l2.get_neuron_array():
-    print(i.get_value(), i.get_weights())
+    print(i.get_value(), i.get_weights(), i.get_bias())
 print(l2.get_num_neurons())
 
 for i in l3.get_neuron_array():
-    print(i.get_value(), i.get_weights())
+    print(i.get_value(), i.get_weights(), i.get_bias())
 print(l3.get_num_neurons())
 
--- neuron.py
+++ neuron.py
@@ -1,19 +1,34 @@
 import random as r
+import math as m
+
+
+def sigmoid(val):
+    val2 = 1 / (1 + m.exp(-val))
+    return val2
 
 
 class Neuron:
     def __init__(self, value=0, connects=None):
-        if connects is None:
-            connects = []
         self.value = value
-        self.weights = []
-        for i in range(0, len(connects)):
-            self.weights.append(r.random())
-        for other_neuron in connects:
-            self.value += other_neuron.get_value()
+        if connects is None:
+            self.connects = []
+        else:
+            self.connects = connects
+            self.weights = []
+            for i in range(0, len(connects)):
+                self.weights.append(2*r.random() - 1)
+            self.bias = 2*r.random() - 1
+
+            for i in range(0, len(connects)):
+                self.value += connects[i].get_value()*self.weights[i]
+            self.value += self.bias
+            self.value = sigmoid(self.value)
 
     def get_value(self):
         return self.value
 
     def get_weights(self):
         return self.weights
+
+    def get_bias(self):
+        return self.bias
