ed7199581a7a8fa7a8f2c6d544db2c2ad7b372f6
JM5064 <thisisjustinmao@gmail.com>
2022-10-27 10:14:12 -0400

    root mean squared error

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import math
+
 import idx2numpy
 import layer as la
 import neuron as n
+from sklearn.metrics import mean_squared_error
 
 train_images = idx2numpy.convert_from_file("train-images-idx3-ubyte")
 train_labels = idx2numpy.convert_from_file("train-labels-idx1-ubyte")
@@ -15,6 +18,10 @@ def create_grayscale_array():
                 grayscale_array.append(img[r][c] / 255)
         arr.append(grayscale_array)
     return arr
+
+
+def calculate_mean_squared_error(actuals, predicts):
+    return mean_squared_error(actuals, predicts, squared=False)
 
 
 l1 = la.Layer(None, val_array=train_images[0])
@@ -36,12 +43,12 @@ for i in l4.get_neuron_array()[0]:
     print(i.get_value(), i.get_weights(), i.get_bias())
 
 
-for i in range(1, len(train_images)):
-    l1.update(train_images[i])
-    l2.update()
-    l3.update()
-    l4.update()
-    print(i)
+# for i in range(1, len(train_images)):
+#     l1.update(train_images[i])
+#     l2.update()
+#     l3.update()
+#     l4.update()
+#     print(i)
 
 for i in l2.get_neuron_array()[0]:
     print(i.get_value(), i.get_weights(), i.get_bias())
@@ -51,3 +58,18 @@ for i in l3.get_neuron_array()[0]:
 
 for i in l4.get_neuron_array()[0]:
     print(i.get_value(), i.get_weights(), i.get_bias())
+
+print()
+
+actuals = []
+predicts = []
+
+for i in l4.get_neuron_array()[0]:
+    if i == int(train_labels[0]):
+        actuals.append(0)
+    else:
+        actuals.append(1)
+    predicts.append(i.get_value())
+
+print(calculate_mean_squared_error(actuals, predicts))
+
