47fdcff3f53cd6aa8d717728ce659b21263e33ce
JM5064 <thisisjustinmao@gmail.com>
2022-11-18 19:41:32 -0500

    possibly faulty drawing code

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import numpy as np
 import layer as la
 import neuron as n
 from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+
+import pygame
+
+from pygame.locals import (
+    QUIT,
+    MOUSEBUTTONUP,
+    MOUSEBUTTONDOWN
+)
 
 train_images = idx2numpy.convert_from_file("train-images-idx3-ubyte")
 train_labels = idx2numpy.convert_from_file("train-labels-idx1-ubyte")
@@ -11,7 +20,7 @@ test_images = idx2numpy.convert_from_file("t10k-images-idx3-ubyte")
 test_labels = idx2numpy.convert_from_file("t10k-labels-idx1-ubyte")
 
 set_size = 100
-set_num = 60000//set_size
+set_num = 60000 // set_size
 
 
 def dsigmoid(x):
@@ -91,8 +100,8 @@ def write_test_data_to_file():
                 outfile.write('# New slice\n')
 
 
-write_train_data_to_file()  # get rid of these when run once
-write_test_data_to_file()
+# write_train_data_to_file()  # get rid of these when run once
+# write_test_data_to_file()
 
 
 def get_train_vect_arr():
@@ -156,102 +165,104 @@ l2 = la.Layer(100, l1, weights=get_weights(2), biases=np.rot90([get_biases(2)], 
 l3 = la.Layer(10, l2, weights=get_weights(3), biases=np.rot90([get_biases(3)], 3))
 
 
-prop_c = 2.0
+prop_c = 1.0
 
 
-for j in range(600):
-    train_vect = train_vect_arr[j % set_num]
-    total = 0
-    l3_wshifts = np.empty([l3.num_neurons, l3.prev_len])
-    l3_wshifts.fill(0)
-    l3_bshifts = np.empty([l3.num_neurons, 1])
-    l3_bshifts.fill(0)
-    l2_wshifts = np.empty([l2.num_neurons, l2.prev_len])
-    l2_wshifts.fill(0)
-    l2_bshifts = np.empty([l2.num_neurons, 1])
-    l2_bshifts.fill(0)
-    l1_wshifts = np.empty([l1.num_neurons, l1.prev_len])
-    l1_wshifts.fill(0)
-    l1_bshifts = np.empty([l1.num_neurons, 1])
-    l1_bshifts.fill(0)
-
-    for i in range(len(train_vect)):
-        l1.update(train_vect[i])
-        l2.update()
-        l3.update()
-        total += MSE((j % set_num) * set_size + i)
-
-        da3cost = []
-        da2cost = []
-        da1cost = []
-        for k in range(l1.num_neurons):
-            da1cost.append(0)
-        for k in range(l2.num_neurons):
-            da2cost.append(0)
-        d_a_to_cost((j % set_num) * set_size + i)
-        dza1 = dsigmoid(l1.prod)
-        dza2 = dsigmoid(l2.prod)
-        dza3 = dsigmoid(l3.prod)
-
-        layer = l3
-        matrix = l3.matrix
-        prev_matrix = layer.prev_matrix
-        for m in range(layer.num_neurons):
-            dz3costm = da3cost[m] * dza3[m, 0]
-            for n in range(layer.prev_len):
-                l3_wshifts[m, n] -= dz3costm * prev_matrix[n, 0]
-                da2cost[n] += dz3costm * layer.weights[m, n]
-            l3_bshifts[m, 0] -= dz3costm
-
-        layer = l2
-        matrix = prev_matrix
-        prev_matrix = layer.prev_matrix
-        for m in range(l2.num_neurons):
-            dz2costm = da2cost[m] * dza2[m, 0]
-            for n in range(l2.prev_len):
-                l2_wshifts[m, n] -= dz2costm * prev_matrix[n, 0]
-                da1cost[n] += dz2costm * layer.weights[m, n]
-            l2_bshifts[m, 0] -= dz2costm
-
-        layer = l1
-        matrix = prev_matrix
-        prev_matrix = layer.prev_matrix
-        for m in range(l1.num_neurons):
-            dz1costm = da1cost[m] * dza1[m, 0]
-            for n in range(l1.prev_len):
-                l1_wshifts[m, n] -= dz1costm * prev_matrix[n, 0]
-            l1_bshifts[m, 0] -= dz1costm
-
-    avg = total / len(train_vect)
-    # print(10 * avg)
-    if j % 10 == 0:
-        print(f'{j} {10 * avg}')
-
-    l3_wshifts /= len(train_vect)
-    l3_wshifts *= prop_c
-    l3_bshifts /= len(train_vect)
-    l3_bshifts *= prop_c
-    l2_wshifts /= len(train_vect)
-    l2_wshifts *= prop_c
-    l2_bshifts /= len(train_vect)
-    l2_bshifts *= prop_c
-    l1_wshifts /= len(train_vect)
-    l1_wshifts *= prop_c
-    l1_bshifts /= len(train_vect)
-    l1_bshifts *= prop_c
-
-    l3.change_weights(l3_wshifts)
-    l3.change_biases(l3_bshifts)
-    l2.change_weights(l2_wshifts)
-    l2.change_biases(l2_bshifts)
-    l1.change_weights(l1_wshifts)
-    l1.change_biases(l1_bshifts)
+# for j in range(600):
+#     train_vect = train_vect_arr[j % set_num]
+#     total = 0
+#     l3_wshifts = np.empty([l3.num_neurons, l3.prev_len])
+#     l3_wshifts.fill(0)
+#     l3_bshifts = np.empty([l3.num_neurons, 1])
+#     l3_bshifts.fill(0)
+#     l2_wshifts = np.empty([l2.num_neurons, l2.prev_len])
+#     l2_wshifts.fill(0)
+#     l2_bshifts = np.empty([l2.num_neurons, 1])
+#     l2_bshifts.fill(0)
+#     l1_wshifts = np.empty([l1.num_neurons, l1.prev_len])
+#     l1_wshifts.fill(0)
+#     l1_bshifts = np.empty([l1.num_neurons, 1])
+#     l1_bshifts.fill(0)
+#
+#     for i in range(len(train_vect)):
+#         l1.update(train_vect[i])
+#         l2.update()
+#         l3.update()
+#         total += MSE((j % set_num) * set_size + i)
+#
+#         da3cost = []
+#         da2cost = []
+#         da1cost = []
+#         for k in range(l1.num_neurons):
+#             da1cost.append(0)
+#         for k in range(l2.num_neurons):
+#             da2cost.append(0)
+#         d_a_to_cost((j % set_num) * set_size + i)
+#         dza1 = dsigmoid(l1.prod)
+#         dza2 = dsigmoid(l2.prod)
+#         dza3 = dsigmoid(l3.prod)
+#
+#         layer = l3
+#         matrix = l3.matrix
+#         prev_matrix = layer.prev_matrix
+#         for m in range(layer.num_neurons):
+#             dz3costm = da3cost[m] * dza3[m, 0]
+#             for n in range(layer.prev_len):
+#                 l3_wshifts[m, n] -= dz3costm * prev_matrix[n, 0]
+#                 da2cost[n] += dz3costm * layer.weights[m, n]
+#             l3_bshifts[m, 0] -= dz3costm
+#
+#         layer = l2
+#         matrix = prev_matrix
+#         prev_matrix = layer.prev_matrix
+#         for m in range(l2.num_neurons):
+#             dz2costm = da2cost[m] * dza2[m, 0]
+#             for n in range(l2.prev_len):
+#                 l2_wshifts[m, n] -= dz2costm * prev_matrix[n, 0]
+#                 da1cost[n] += dz2costm * layer.weights[m, n]
+#             l2_bshifts[m, 0] -= dz2costm
+#
+#         layer = l1
+#         matrix = prev_matrix
+#         prev_matrix = layer.prev_matrix
+#         for m in range(l1.num_neurons):
+#             dz1costm = da1cost[m] * dza1[m, 0]
+#             for n in range(l1.prev_len):
+#                 l1_wshifts[m, n] -= dz1costm * prev_matrix[n, 0]
+#             l1_bshifts[m, 0] -= dz1costm
+#
+#     avg = total / len(train_vect)
+#     # print(10 * avg)
+#     if j % 10 == 0:
+#         print(f'{j} {10 * avg}')
+#
+#     l3_wshifts /= len(train_vect)
+#     l3_wshifts *= prop_c
+#     l3_bshifts /= len(train_vect)
+#     l3_bshifts *= prop_c
+#     l2_wshifts /= len(train_vect)
+#     l2_wshifts *= prop_c
+#     l2_bshifts /= len(train_vect)
+#     l2_bshifts *= prop_c
+#     l1_wshifts /= len(train_vect)
+#     l1_wshifts *= prop_c
+#     l1_bshifts /= len(train_vect)
+#     l1_bshifts *= prop_c
+#
+#     l3.change_weights(l3_wshifts)
+#     l3.change_biases(l3_bshifts)
+#     l2.change_weights(l2_wshifts)
+#     l2.change_biases(l2_bshifts)
+#     l1.change_weights(l1_wshifts)
+#     l1.change_biases(l1_bshifts)
 
 
 correct = 0
 tot = 0
 
 for i in range(10000):
+    if i == 0:
+        print(test_vect_arr[0][0])
     l1.update(test_vect_arr[i // set_size][i % set_size])
     l2.update()
     l3.update()
@@ -261,10 +272,12 @@ for i in range(10000):
         if l3.matrix[j, 0] > maxim:
             maxim = l3.matrix[j, 0]
             maxim_index = j
-    print(maxim_index)
-    print(test_labels[i])
+    print(f'{maxim_index} {test_labels[i]}')
+    # print(test_labels[i])
     if maxim_index == test_labels[i]:
         correct += 1
+    else:
+        print("!!!")
     tot += 1
 percent_correct = correct/tot
 if percent_correct > float(np.loadtxt("percentcorrect.txt")) and tot == 10000:
@@ -273,5 +286,66 @@ if percent_correct > float(np.loadtxt("percentcorrect.txt")) and tot == 10000:
 #     update_text_files()
 
 print(f'{correct} / {tot}')
-print(np.rot90([get_biases(1)], 3))
 
+WIDTH, HEIGHT = 700, 700
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen.fill((255, 255, 255))
+
+pixels = []
+
+
+def create_pixel_array():
+    for r in range(WIDTH):
+        pixels.append([])
+        for c in range(HEIGHT):
+            pixels[r].append(screen.get_at((r, c)))
+    return pixels
+
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if pygame.mouse.get_pressed()[0]:
+            pygame.draw.circle(screen, (0, 0, 0), (pygame.mouse.get_pos()), 30)
+        elif event.type == QUIT:
+            running = False
+            create_pixel_array()
+
+    pressed_keys = pygame.key.get_pressed()
+
+    pygame.display.flip()
+
+
+def process_image():
+    pixel_size = 25
+    pixelated = np.zeros((28, 28))
+    for r in range(28):
+        for c in range(28):
+
+            av_color = 0
+            for i in range(r * pixel_size, r * pixel_size + pixel_size):
+                for j in range(c * pixel_size, c * pixel_size + pixel_size):
+                    av_color += pixels[i][j][0]
+            pixelated[r][c] = (255 - (av_color / (pixel_size ** 2))) / 255
+    pxls = []
+    for pxl in create_one_dimensional(pixelated):
+        pxls.append([pxl])
+
+    return np.array(pxls)
+
+
+print(process_image())
+
+l1.update(process_image())
+l2.update()
+l3.update()
+
+maxim = 0
+maxim_index = 0
+for j in range(len(l3.matrix)):
+    if l3.matrix[j, 0] > maxim:
+        maxim = l3.matrix[j, 0]
+        maxim_index = j
+print(maxim_index)
+plt.imshow(train_images[0], cmap=plt.cm.binary)
